db62ab5813af67307ed7fdfe6f95a5a4b4d57afa
Esha Peddireddy <esha@Eshas-MacBook-Pro.local>
2018-07-15 12:00:35 -0700

    logged all the buttons

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">HOT or COLD</h1>
-          <button onClick="aboutApp()">WHAT?</button>
-          <button onClick="resetApp()">+NEW GAME</button>
+          <button onClick={() => console.log("what")}>WHAT?</button>
+          <button onClick={() => console.log("new game")}>+NEW GAME</button>
         </header>
         <div className="guess-box">
           <h2>Hot or cold(tochange)</h2>
           <p>Enter your guess(to change to input)</p>
-          <button onClick="">Guess button(nothing hooked to onclick)</button>
+          <button onClick={() => console.log("guess buitton")} >Guess button(nothing hooked to onclick)</button>
           <h3>Guess # <strong>7(to change to counter)!</strong></h3>
           <p>(list previous guesses here)</p>
         </div>
